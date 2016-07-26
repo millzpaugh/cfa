f907e313333a31e7c4ab73db73b83825eaf1edbd
Ann Millspaugh <ann.millspaugh@gmail.com>
2016-07-26 16:44:59 -0700

    add 3 hour weather increments

--- scripts/weather.js
+++ scripts/weather.js
@@ -7,27 +7,57 @@ function retrieveWeatherData(param, location){
 		dataType: 'json', 
 		success: function(response){
 			weatherData = response; 
-			displayWeatherData(response); 
+			if (param === 'weather'){
+				displayWeatherData(response); 
+			}else if(param === 'forecast'){
+				displayForecastData(response); 
+			}
+
 		}
 	})
 }; 
 
 function displayWeatherData(){
-    degreesF = Math.round((weatherData.main.temp- 273) * 9/5 + 32);
+	var cityName = weatherData.name.replace(' ', '')
+	var cityForecast =  $("<ul id=" + cityName + "><h3>" + weatherData.name + "</h3></ul>");
+	$("div#forecast").append(cityForecast); 
+    var degreesF = Math.round((weatherData.main.temp- 273) * 9/5 + 32);
     var temp =  $("<li class='temp'> It is currently " + degreesF + " degrees Fahrenheit in " + weatherData.name + " with " + weatherData.weather[0].description + ".</li>");
-    $("#weather-info").append(temp); 
-
+    $("ul#" + cityName).append(temp); 
 }
 
-function retrieveWeatherDataForAllLocations(param, locations){
+function displayForecastData(){
+	var d = new Date();
+	var n = d.getHours();
+	var index = Math.round(n/3, 2)
+	for (i=index;i<index+3;i++){
+		var cityName = weatherData.city.name.replace(' ', '')
+		var date = weatherData.list[i].dt_txt.slice(0, 16)
+		var description = weatherData.list[i].weather[0].description
+		var degreesF = Math.round((weatherData.list[i].main.temp- 273) * 9/5 + 32);
+    	var forecast =  $("<li class='temp'>On " + date + " it will be " + degreesF + " degrees Fahrenheit in " + weatherData.city.name + " with " + description + ".</li>");
+    	$("ul#" + cityName).append(forecast); 
+	}    
+}
+
+function retrieveWeatherDataForAllLocations(param, locations, callback){
+	for (i=0;i<locations.length;i++){
+		retrieveWeatherData(param, locations[i])
+	}
+
+	retrieveForecastDataForAllLocations('forecast', locations); 
+}
+
+function retrieveForecastDataForAllLocations(param, locations){
 	for (i=0;i<locations.length;i++){
 		retrieveWeatherData(param, locations[i])
 	}
 }
 
 $(function(){
-	$("#weather-info").empty()
+	$("div#forecast").empty()
 	var locations = ["Oakland", "San Francisco", "San Jose"]; 
 	retrieveWeatherDataForAllLocations('weather', locations); 
+	
 })
 

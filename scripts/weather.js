7e4b92c546b071be14956896f8591efe87dc4ab7
Ann Millspaugh <ann.millspaugh@gmail.com>
2016-07-26 14:54:57 -0700

    add api param

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,8 +1,8 @@
 var weatherData; 
 
-function retrieveWeatherData(location){
+function retrieveWeatherData(param, location){
 	$.ajax({
-		url: 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=dd6d612e24b7d9caa94e93448aaea752', 
+		url: 'http://api.openweathermap.org/data/2.5/' + param + '?q=' + location + '&APPID=dd6d612e24b7d9caa94e93448aaea752', 
 		type: 'GET', 
 		dataType: 'json', 
 		success: function(response){
@@ -13,21 +13,21 @@ function retrieveWeatherData(location){
 }; 
 
 function displayWeatherData(){
-	console.log(weatherData.main.temp)
     degreesF = Math.round((weatherData.main.temp- 273) * 9/5 + 32);
-    temp =  $("<li class='temp'> It is currently " + degreesF + " degrees Fahrenheit in " + weatherData.name + " with " + weatherData.weather[0].description + ".</li>");
+    var temp =  $("<li class='temp'> It is currently " + degreesF + " degrees Fahrenheit in " + weatherData.name + " with " + weatherData.weather[0].description + ".</li>");
     $("#weather-info").append(temp); 
 
 }
 
-function retrieveWeatherDataForAllLocations(locations){
+function retrieveWeatherDataForAllLocations(param, locations){
 	for (i=0;i<locations.length;i++){
-		retrieveWeatherData(locations[i])
+		retrieveWeatherData(param, locations[i])
 	}
 }
 
 $(function(){
+	$("#weather-info").empty()
 	var locations = ["Oakland", "San Francisco", "San Jose"]; 
-	retrieveWeatherDataForAllLocations(locations); 
+	retrieveWeatherDataForAllLocations('weather', locations); 
 })
 
